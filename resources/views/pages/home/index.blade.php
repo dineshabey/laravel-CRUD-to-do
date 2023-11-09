c72ba074fb6f4cc5269c3a9ceee517af50c7791c
Dinesh <dinabeysinghe@gmail.com>
2023-11-09 14:43:13 +0530

    add boostrap,nav bar

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col sm 8">Home Page</div>
-            <div class="col sm 4">
-                <button type="button" class="btn btn-success">Test</button>
+            <div class="col sm 12 text-center">
+                <h1 class="page-title">Home Page</h1>
             </div>
+
         </div>
     </div>
 @endsection
