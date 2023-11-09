e4662cba3c982fbf9fa461aa6436a849b9f35836
Dinesh <dinabeysinghe@gmail.com>
2023-11-09 14:57:35 +0530

    JS and CSS stack and push

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -4,9 +4,20 @@
     <div class="container">
         <div class="row">
             <div class="col sm 12 text-center">
-                <h1 class="page-title">Home Page</h1>
+                <h1 class="page-title aa">Home Page</h1>
             </div>
 
         </div>
     </div>
 @endsection
+
+
+@push('css')
+    <style>
+        .aa {
+
+            padding: 40px;
+            color: green;
+        }
+    </style>
+@endpush
