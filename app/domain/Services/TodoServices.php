34488e40194745b9477b1270f1f4b9ef0e4b062c
Dinesh <dinabeysinghe@gmail.com>
2023-11-12 10:11:16 +0530

    add ToDoServices page

--- app/domain/Services/TodoServices.php
+++ app/domain/Services/TodoServices.php
@@ -2,7 +2,40 @@
 
 namespace domain\Services;
 
+use App\Models\Todo;
+
 class TodoServices
 {
+    protected $task;
+    public function __construct()
+    {
+        $this->task = new Todo();
+    }
 
+    public function all()
+    {
+        return $this->task->all();
+    }
+    public function store($data)
+    {
+        $this->task->create($data);
+    }
+    public function delete($id)
+    {
+        $stack = $this->task->find($id);
+        $stack->delete();
+    }
+
+    public function statusUpdate($task_id)
+    {
+        $task = $this->task->find($task_id);
+        $task->done = 1;
+        $task->update();
+    }
+    public function update($id)
+    {
+
+
+        return redirect()->back();
+    }
 }
