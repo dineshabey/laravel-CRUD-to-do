645e63c1db2c6f73c5de69e4c426a839ec211117
Dinesh <dinabeysinghe@gmail.com>
2023-11-15 12:26:49 +0530

    created banner blade file and change controllers

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -3,47 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use domain\Facades\TodoFacade;
+use domain\Facades\BannerFacade;
 
 class BannerController extends ParentController
 {
     public function index()
     {
-        $response['tasks'] = TodoFacade::all();
-        return view('pages.todo.todoIndex')->with($response);
+        $response['tasks'] = BannerFacade::all();
+        return view('pages.banner.banner')->with($response);
     }
     public function store(Request $request)
     {
 
-        TodoFacade::store($request->all());
+        BannerFacade::store($request->all());
         return redirect()->back();
     }
     public function delete($banner_id)
     {
-        TodoFacade::delete($banner_id);
+        BannerFacade::delete($banner_id);
         return redirect()->back();
     }
     public function statusUpdate($banner_id)
     {
-        TodoFacade::statusUpdate($banner_id);
+        BannerFacade::statusUpdate($banner_id);
         return redirect()->back();
     }
     public function edit(Request $request)
     {
-        $response['task']  = TodoFacade::get($request['banner_id']);
+        $response['task']  = BannerFacade::get($request['banner_id']);
         // dd($request);
         return view('pages.todo.edit')->with($response);
     }
     public function update(Request $request, $banner_id)
     {
 
-        TodoFacade::update($request->all(), $banner_id);
+        BannerFacade::update($request->all(), $banner_id);
         return redirect()->back();
     }
 
     public function get($banner_id)
     {
-        $response['task'] = TodoFacade::find($banner_id);
+        $response['task'] = BannerFacade::find($banner_id);
         return view('pages.todo.todoEdit')->with($response);
     }
 }
---
+++ resources/views/pages/banner/banner.blade.php
@@ -0,0 +1,113 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col sm-12 text-center">
+                <h1 class="page-title">
+                    Banner Images
+                </h1>
+            </div>
+
+            <form action="{{ route('banner.store') }}" method="post">
+                @csrf
+                <div class="row">
+                    <div class="col sm-8">
+                        <div class="form-group">
+                            <input class="form-control" type="text" placeholder="Enter Banner Title" name="title">
+                        </div>
+                    </div>
+                    <div class="col sm-8">
+                        <div class="form-group">
+                            <input class="form-control" type="file" placeholder="Enter Banner Title" name="images"
+                                accept="image/jpg,image/png">
+                        </div>
+                    </div>
+                    <div class="col sm-4">
+                        <button class="btn btn-success" type="submit" name="submit">Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="row mt-5">
+            <div class="col-sm-12">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Task</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($tasks as $key => $task)
+                            <tr>
+                                <th scope="row">{{ $key }}</th>
+                                <td>{{ $task->title }}</td>
+                                <td>
+                                    @if ($task->done == 1)
+                                        <span class="badge text-bg-success">Completed</span>
+                                    @else
+                                        <span class="badge text-bg-warning">Not completed</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                                        <a type="button" class="btn btn-danger"
+                                            href= '{{ route('banner.delete', $task->id) }}''>Delete</a>
+                                        <a type="button" class="btn btn-success"
+                                            href={{ route('banner.updateStatus', $task->id) }}>Update</a>
+
+                                        <a type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                            onclick="taskEditModal({{ $task->id }})">
+                                            Edit
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        @include('components.popup.popup')
+
+    </div>
+@endsection
+
+@push('css')
+    <style>
+        .page-title {
+            padding-top: 5vh;
+            font-size: 5rem;
+            color: rgb(66, 202, 48);
+        }
+    </style>
+@endpush
+
+
+@push('js')
+    <script>
+        function taskEditModal(task_id) {
+            var data = {
+                task_id: task_id,
+            };
+
+            $.ajax({
+                url: '{{ route('banner.edit') }}',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                type: 'GET',
+                dataType: '',
+                data: data,
+                success: function(response) {
+                    $('#taskEdit').modal('show');
+                    $('#taskEditContent').html(response);
+                }
+            });
+        }
+    </script>
+@endpush
